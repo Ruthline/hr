065b69694e4c1ab21e9816ede2a7fe744ac3c187
Ruthline <rutheveline@hotmail.es>
2023-11-26 23:36:10 -0500

    BD conected to hosting with errors for the procedures

--- modelo/configuracion.php
+++ modelo/configuracion.php
@@ -2,9 +2,9 @@
 	// Nombramos la URL de nuestro Servidor (Web/Local)
 	$host="localhost";
 	// Nombramos nuestra BD
-	$db="hr";
+	$db="id21579218_healthy";
 	// Nombramos nuestro USER de la BD
-	$user="root";
+	$user="id21579218_renewal";
 	// Nombramos nuestra CONTRASEÑA del USER BD
-	$pass='';	
+	$pass='Healthyrenewal2*';	
 ?>
